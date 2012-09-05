b2c8415150059751d63823fafc0532dced359674
Dave Ward <dave@encosia.com>
2012-09-04 22:15:45 -0400

    Lightly-tested support for multiple DBGeo fields on the same page.

--- SpatialFun/Scripts/EFSpatialGoogleMaps.js
+++ SpatialFun/Scripts/EFSpatialGoogleMaps.js
@@ -1,56 +1,65 @@
 ﻿(function() {
-  // Find the textbox and cache it for later.
-  var $input = $('.editor-for-dbgeography, .display-for-dbgeography'),
-      latitude = $input.data('latitude'),
-      longitude = $input.data('longitude');
+  // Method signature matching $.fn.each()'s, for easy use in the .each loop later.
+  var initialize = function(i, el) {
+    // el is the input element that we need to initialize a map for, jQuery-ize it,
+    //  and cache that since we'll be using it a few times.
+    var $input = $(el),
+        latitude = $input.data('latitude'),
+        longitude = $input.data('longitude');
 
-  // Create the map div and insert it into the page.
-  var $map = $('<div>', {
-    css: {
-      width: '400px',
-      height: '400px'
-    }
-  }).insertAfter($input);
+    // Create the map div and insert it into the page.
+    var $map = $('<div>', {
+      css: {
+        width: '400px',
+        height: '400px'
+      }
+    }).insertAfter($input);
 
-  // Create a "Google(r)(tm)" LatLong object representing our DBGeometry's lat/long.
-  var position = new google.maps.LatLng(latitude, longitude);
+    // Create a "Google(r)(tm)" LatLong object representing our DBGeometry's lat/long.
+    var position = new google.maps.LatLng(latitude, longitude);
 
-  var mapOptions = {
-    zoom: 14,
-    center: position,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    maxZoom: 14
-  };
+    var mapOptions = {
+      zoom: 14,
+      center: position,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      maxZoom: 14
+    };
 
-  // Initialize the map widget.
-  var map = new google.maps.Map($map[0], mapOptions);
+    // Initialize the map widget.
+    var map = new google.maps.Map($map[0], mapOptions);
 
-  // Place a marker on it, representing the DBGeometry object's position.
-  var marker = new google.maps.Marker({
-    position: position,
-    map: map
-  });
-
-  var updateMarker = function(updateEvent) {
-    marker.setPosition(updateEvent.latLng);
-
-    map.panTo(updateEvent.latLng);
-
-    // Black magic, courtesy of Hanselman's original version.
-    $input.val(marker.getPosition().toUrlValue(13));
-  };
-
-  // If the input came from an EditorFor, initialize editing-related events.
-  if ($input.hasClass('editor-for-dbgeography')) {
-    google.maps.event.addListener(map, 'click', updateMarker);
-
-    // Attempt to react to user edits in the input field.
-    $input.on('change', function(evt) {
-      var latLong = this.value.match(/-?\d+\.\d+/g);
-
-      latLong = new google.maps.LatLng(latLong[0], latLong[1]);
-
-      updateMarker({ latLng: latLong });
+    // Place a marker on it, representing the DBGeometry object's position.
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
     });
-  }
+
+    var updateMarker = function(updateEvent) {
+      marker.setPosition(updateEvent.latLng);
+
+      map.panTo(updateEvent.latLng);
+
+      // Black magic, courtesy of Hanselman's original version.
+      $input.val(marker.getPosition().toUrlValue(13));
+    };
+
+    // If the input came from an EditorFor, initialize editing-related events.
+    if ($input.hasClass('editor-for-dbgeography')) {
+      google.maps.event.addListener(map, 'click', updateMarker);
+
+      // Attempt to react to user edits in the input field.
+      $input.on('change', function(evt) {
+        var latLong = this.value.match(/-?\d+\.\d+/g);
+
+        latLong = new google.maps.LatLng(latLong[0], latLong[1]);
+
+        updateMarker({ latLng: latLong });
+      });
+    }
+  };
+
+  // Find the textbox and cache it for later.
+  var $inputs = $('.editor-for-dbgeography, .display-for-dbgeography');
+
+  $inputs.each(initialize);
 })();
