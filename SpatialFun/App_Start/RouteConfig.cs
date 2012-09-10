4b11d8ef931e2824535199c51c0dd11693393345
Scott Hanselman <scott@hanselman.com>
2012-09-10 14:21:13 -0700

    Inital merge of WebForms and MVC

--- SpatialFun/App_Start/RouteConfig.cs
+++ SpatialFun/App_Start/RouteConfig.cs
@@ -14,24 +14,22 @@ namespace MvcApplication2
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.EnableFriendlyUrls();
-
-            ////webforms routes
+            // Manually created webforms routes. I'll prefer to use FriendlyUrls
             //RouteTable.Routes.MapPageRoute("TouristAttraction", "WF", "~/WebForms/Default.aspx");
             //RouteTable.Routes.MapPageRoute("TouristAttraction2", "WF/Attraction", "~/WebForms/Default.aspx");
             //RouteTable.Routes.MapPageRoute("TouristAttractionNew", "WF/Attraction/Create", "~/WebForms/Create.aspx");
             //RouteTable.Routes.MapPageRoute("TouristAttractionEdit", "WF/Attraction/Edit/{id}", "~/WebForms/Edit.aspx");
             //RouteTable.Routes.MapPageRoute("TouristAttractionDelete", "WF/Attraction/Delete/{id}", "~/WebForms/Delete.aspx");
             //RouteTable.Routes.MapPageRoute("TouristAttractionDetails", "WF/Attraction/Details/{id}", "~/WebForms/Details.aspx");
-            
   
-            //MVC will be the default, WF is under /WF/
+            //MVC will be for MVC, while WebForms is under /WebForms/ using Friendly URLs
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "MVC/{controller}/{action}/{id}",
                 defaults: new { controller = "Attraction", action = "Index", id = UrlParameter.Optional }
             );
-          
+
+            routes.EnableFriendlyUrls();
         }
     }
 }
